556465b376fcd61e48e7df7fe7c73d9a2ac536fe
LordR3ne <renehill170@gmail.com>
2025-10-24 09:40:38 +0200

    Droid Test

--- Droid/Droid.js
+++ Droid/Droid.js
@@ -1,1 +1,16 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.Droid = void 0;
+var Droid;
+(function (Droid) {
+    function getCommand() {
+        const command = {
+            module: "Chassis",
+            method: "move",
+            data: "left",
+        };
+        console.log(command);
+        return command;
+    }
+    Droid.getCommand = getCommand;
+})(Droid || (exports.Droid = Droid = {}));
