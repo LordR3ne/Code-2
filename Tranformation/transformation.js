98600f727ce3ad26953d28794785040d89610324
LordR3ne <renehill170@gmail.com>
2025-10-27 16:25:40 +0100

    CHanges to Transformation

--- Tranformation/transformation.js
+++ Tranformation/transformation.js
@@ -43,18 +43,24 @@ var transformation;
         canvas.height = window.innerHeight;
         crc2.fillStyle = "#07fffbff";
         crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
+
         crc2.save();
         crc2.translate(0, 0);
         transform(crc2, "black");
+        console.log("Original Coordinate System in Black", crc2.getTransform)
         crc2.restore();
+
         crc2.save();
         crc2.translate(200, 200);
         transform(crc2, "green");
+        console.log("Transaltet Version in Green", crc2.getTransform)
         crc2.restore();
+
         crc2.save();
         crc2.rotate(Math.PI);
         crc2.translate(-450, -450);
         transform(crc2, "red");
+        console.log("Rotated and translated in red", crc2.getTransform)
         crc2.restore();
     }
 })(transformation || (transformation = {}));
